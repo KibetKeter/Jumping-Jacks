71ceafe78efaaad49d8cded2090825b5a077da9b
Richard-gm75 <Richard.Muriithi@strathmore.edu>
2020-07-20 16:27:19 +0300

    created donations,stocks,order,supplier,donor, accounts

---
+++ app/Http/Controllers/DonorAccountsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Donor_Accounts;
+use Illuminate\Http\Request;
+
+class DonorAccountsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Donor_Accounts  $donor_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Donor_Accounts $donor_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Donor_Accounts  $donor_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Donor_Accounts $donor_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Donor_Accounts  $donor_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Donor_Accounts $donor_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Donor_Accounts  $donor_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Donor_Accounts $donor_Accounts)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/SupplierAccountsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Supplier_Accounts;
+use Illuminate\Http\Request;
+
+class SupplierAccountsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Supplier_Accounts  $supplier_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Supplier_Accounts $supplier_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Supplier_Accounts  $supplier_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Supplier_Accounts $supplier_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Supplier_Accounts  $supplier_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Supplier_Accounts $supplier_Accounts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Supplier_Accounts  $supplier_Accounts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Supplier_Accounts $supplier_Accounts)
+    {
+        //
+    }
+}
