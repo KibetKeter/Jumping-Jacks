3e28f64e5ef50abedb856635412a7d46e201109b
Richard-gm75 <Richard.Muriithi@strathmore.edu>
2020-09-10 22:00:33 +0300

    new works

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 Route::get('/generaluser', 'GeneralUserController@index')->name('generaluser.dashboard');
+
+Route::get('/generaluser/supplier', 'SupplierController@index')->name('generaluser.dashboard');
+Route::get('/generaluser/supplier/create', 'SupplierController@index')->name('generaluser.dashboard');
+
+
+Route::get('/generaluser/donor', 'DonorAccountsController@index')->name('generaluser.dashboard');
+Route::get('/generaluserdonor/create', 'DonorAccountsController@index')->name('generaluser.dashboard');
