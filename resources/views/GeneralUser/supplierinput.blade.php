75b7f7307f054397db7d09ea3471a99410a21a5d
Brian Kibet <brian.kipketer@strathmore.edu>
2020-09-11 18:59:59 +0300

    Modified the Input supplier details page a bit

--- resources/views/GeneralUser/supplierinput.blade.php
+++ resources/views/GeneralUser/supplierinput.blade.php
@@ -4,7 +4,7 @@
 						<div class="col-md-12 col-sm-12 ">
 							<div class="x_panel">
 								<div class="x_title">
-									<h2>Input<small><strong>Supplier details</strong></small></h2>
+									<h2>Input Supplier details</h2>
 								<div class="clearfix"></div>
 								</div>
 								<div class="x_content">
@@ -61,9 +61,9 @@
 									<div class="ln_solid"></div>
 										<div class="item form-group">
 											<div class="col-md-6 col-sm-6 offset-md-3">
-												<button class="btn btn-primary" type="button">Cancel</button>
-												<button class="btn btn-primary" type="reset">Reset</button>
-												<button href= type="submit" class="btn btn-success">Submit</button>
+												<a href="{{url('/generaluser')}}" class="btn btn-primary" type="button">Back to General User Page</a>
+												<button class="btn btn-primary" type="reset">Reset the form</button>
+												<button type="submit" class="btn btn-success">Submit the form</button>
 											</div>
 										</div>
 
