87bc0d4f60ff5b402cf00c0eec6ecf39728305d4
Brian Kibet <brian.kipketer@strathmore.edu>
2020-09-14 21:31:50 +0300

    Small Changes to the UI Elements

--- resources/views/Admin/supplierinput.blade.php
+++ resources/views/Admin/supplierinput.blade.php
@@ -62,10 +62,10 @@
 									<div class="ln_solid"></div>
 										<div class="item form-group">
 											<div class="col-md-6 col-sm-6 offset-md-3">
-												<a href="{{url('/generaluser')}}" class="btn btn-primary" type="button">Back to General User Page</a>
+												<button type="submit" class="btn btn-success">Submit the form</button><br/>
 												<button class="btn btn-primary" type="reset">Reset the form</button>
-												<button type="submit" class="btn btn-success">Submit the form</button>
-												<a href="{{url('/admin/supplierinput/table') }}" type="button" class="btn btn-primary">View Suppliers</a>
+												<a href="{{url('/admin/supplierinput/table') }}" type="button" class="btn btn-primary">View Suppliers</a><br/>
+												<a href="{{url('/generaluser')}}" class="btn btn-primary" type="button">Back to General User Page</a><br/>
 											</div>
 										</div>
 
