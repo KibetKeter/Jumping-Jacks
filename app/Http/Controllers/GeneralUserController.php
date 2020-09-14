e48f1918ef60d976fcffff05762f7280e87e3a0c
Richard-gm75 <Richard.Muriithi@strathmore.edu>
2020-09-14 12:35:57 +0300

    solved error in generaluser controller

--- app/Http/Controllers/GeneralUserController.php
+++ app/Http/Controllers/GeneralUserController.php
@@ -75,7 +75,7 @@ public function input_donations()
         $donations = new Donation();
         $donations->donor_name  = request('donor_name');
         $donations->description = request('description');
-        $donations->description = request('amount');
+        $donations->amount = request('amount');
         $donations->save();
     return redirect()->route('input_donations')->withSuccess(['Donations have been Recorded Successfully👍🏿']);
 
