82a9aeac3feaf13fa794937808d1f7b98ffceab9
Diana-Kalema <diana.kalema@strathmore.edu>
2020-09-23 14:57:08 +0300

    error resolution

--- resources/views/Admin/inputstock.blade.php
+++ resources/views/Admin/inputstock.blade.php
@@ -20,7 +20,7 @@
 											</ul>
 										</div>
                                     @endif
-									<form action="{{ route('stock.create') }}" method="POST" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+									<form action="{{ route('admin_input_stock.create') }}" method="POST" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
                                         @csrf
 
                                  <div class="item form-group">
@@ -104,7 +104,7 @@
 												<a href="{{url('/admin')}}" class="btn btn-primary" type="button">Back to previous page</a>
 												<button class="btn btn-primary" type="reset">Reset</button>
 												<button type="submit" class="btn btn-success">Submit</button>
-												<a href = "{{ url('/admin/orderstock/table')}}" type="button" class="btn btn-secondary">Click to view Stock inventory</a>
+												<a href = "{{ url('/admin/stockview/table')}}" type="button" class="btn btn-secondary">Click to view Stock inventory</a>
 											</div>
 										</div>
 
